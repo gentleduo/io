65891d2824d580b2facc881ea4fa44969991bcc8
gentleduo <gentleduo@163.com>
2022-01-24 16:43:36 +0800

    update OSFileIO

--- src/OSFileIO.java
+++ src/OSFileIO.java
@@ -21,6 +21,9 @@ public class OSFileIO {
             case "1":
                 testBufferedFileIO();
                 break;
+            case "2":
+                testRandomAccessFileWrite();
+                break;
             default:
                 whatByteBuffer();
         }
@@ -69,7 +72,7 @@ public class OSFileIO {
         // 只有文件系统的的FileChannel上有map方法（本质是通过系统调用，调用内核的mmap方法），如果是serversocket或者是socket上面是没有map方法的，只有文件（块设备）才可以做内存映射。
         // 块设备：是可以来回自由寻址的，能够自由读取文件中前面的某一块或者后面的某一块
         // 内存映射：将内核的pagecache和文件数据页映射起来
-        // map：通过系统调用，使用内核中的mmap方法得到一个堆外的并且和文件映射的bytebuffer
+        // map：通过系统调用，使用内核中的mmap方法得到一个堆外的并且和文件映射的bytebuffer（即：分配的MappedByteBuffer的逻辑地址直接映射到内核的pagecache）
         MappedByteBuffer map = rafChannel.map(FileChannel.MapMode.READ_WRITE, 0, 4096);
         // 这里通过调用MappedByteBuffer的put方法就没有系统调用了，因为MappedByteBuffer和文件做了映射，所以数据会直接到达内核的pagecache。
         // 其他的方式是需要out.write()这样的系统调用才能要进程的数据进入内核的pagecache。也就是说必须有用户态、内核态的切换
@@ -78,14 +81,31 @@ public class OSFileIO {
         map.put("@@@".getBytes());
         System.out.println("map--put---------------------------------");
         System.in.read();
+
+//        map.force(); //类似于flush
+
+        raf.seek(0);
+
+        ByteBuffer buffer = ByteBuffer.allocate(8192);
+        int read = rafChannel.read(buffer);
+        System.out.println(buffer);
+        buffer.flip();
+        System.out.println(buffer);
+
+        for (int i = 0; i < buffer.limit(); i++) {
+            Thread.sleep(200);
+            System.out.print((char) buffer.get(i));
+        }
     }
 
     public static void whatByteBuffer() {
 
         // 通过socket写入的时候，会创建一个堆外的、java进程内的bytebuffer，然后将数据从堆内的bytebuffer拷贝到堆外的bytebuffer，最后写入内核中（因此直接申请堆外的bytebuffer会效率更高）
         // 所以java是必须先从jvm的虚拟堆内将数据拷贝到java进程的堆内然后再将数据拷贝到内核空间中，
-        ByteBuffer buffer = ByteBuffer.allocate(1024);//堆内分配
-//        ByteBuffer buffer = ByteBuffer.allocateDirect(1024);//对外分配
+        // 分配在JVM堆中(java进程启动时通过-Xmx指定的堆空间)。这个空间中字节数组的线性地址不是JAVA进程的线性地址，是JVM的线性地址空间，所以程序无法直接访问，必须通过转换或者将数据拷贝的JAVA进程的堆中才能使用。
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        // 分配在JAVA进程的堆中，对于程序来说，可以直接访问访问这个空间中的数据，因为字节数据的线性地址就是进程的线性地址。
+//        ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
 
         System.out.println("position:" + buffer.position());
         System.out.println("limit:" + buffer.limit());
